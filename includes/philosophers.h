0957bb9be334d5dfcbfb5e735d55f70a18cf3bbd
gucamuze <gucamuze@student.42.fr>
2022-01-26 11:31:35 +0100

    #

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: gucamuze <gucamuze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 13:12:14 by gucamuze          #+#    #+#             */
-/*   Updated: 2022/01/21 18:20:17 by gucamuze         ###   ########.fr       */
+/*   Updated: 2022/01/26 11:30:30 by gucamuze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include <sys/time.h>
 # include <stdlib.h>
 # include <limits.h>
+# include <stdarg.h>
 
 typedef struct s_parsed_args
 {
@@ -56,5 +57,7 @@ t_philos_data				**setup_philos_d(t_parsed_args *args);
 int							atoi_w_return(char *str, int *nb_ptr);
 
 void						*philo_thread(void *ptr);
+
+char						*cat_str(char *str, int nb_of_strs, ...);
 
 #endif
